5f9e306d07c5ba3395bce6f3eba5151a6620b375
ayushneekhar <a.neekhar01@gmail.com>
2021-10-09 22:53:38 +0530

    Updated users structure

--- user.go
+++ user.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
-	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+	Name string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email  string             `json:"email,omitempty" bson:"email,omitempty"`
 	Password  string			 `json:"password,omitempty" bson:"password,omitempty"`
 }
